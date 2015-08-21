7caa1304fdf158f8b756b81323f1e061df2e8188
Carl-Martin <carid389@student.liu.se>
2015-08-21 19:34:59 +0200

    parsed sent and acked

--- test.cc
+++ test.cc
@@ -12,7 +12,7 @@ struct pack{
     float time;
     int seq_nr;
     int ack_nr;
-    int size;
+    int port;
 };
 
 struct SEQ{
@@ -21,75 +21,120 @@ struct SEQ{
     int size;
 };
 
-
 using namespace std;
-int main(){
-    vector<struct pack> packets;
-    struct pack tmp_packet;
 
-    string line;
-    char filename[40];
-    cin.getline(filename, 40);
-
+void parse_input(vector<struct pack> *packets, char* filename)
+{
     ifstream the_file;
     the_file.open(filename);
-    string word;
-    stringstream ss;
     int tmp_int;
+    struct pack tmp_packet;
     float tmp_float;
-    ss.clear();
-getline(the_file, line);
+    string word;
+    string line;
+    stringstream ss;
 
-    //parse file, each packet in vector<struct pack>
+    getline(the_file, line);
     while(the_file){
         getline(the_file, line);
-//cout << line << endl << endl;
         ss << line;
         for(int i = 0; i<9; ++i){
             ss >> word;
-           cout << '(' << i << ')' + word << endl;
+            //cout << '(' << i << ')' + word << endl;
 
             switch (i){
                 case 1:
-                    tmp_float = atof(word.c_str());
-                    //cout << tmp_packet.time << endl;
-                    tmp_packet.time=tmp_float;
+                    tmp_packet.time = atof(word.c_str());
                     break;
                 case  5:
-                    tmp_int = atoi(word.c_str());
-                    tmp_packet.size = tmp_int;
+                    tmp_packet.port = atoi(word.c_str());
                     break;
                 case  6:
-                    tmp_int = atoi(word.c_str());
-                    tmp_packet.seq_nr = tmp_int;
+                    tmp_packet.seq_nr = atoi(word.c_str());
                     break;
                 case  7:
-                    tmp_int = atoi(word.c_str());
-                    tmp_packet.ack_nr = tmp_int;
+                    tmp_packet.ack_nr = atoi(word.c_str());
                     break;
             }
-            
+
         }
         ss.str("");
-//        ss.clear();
-        packets.push_back(tmp_packet);
+        packets->push_back(tmp_packet);
     }
     the_file.close();
-    /*
-       tmp_packet.seq_nr = -1;
-       packets.push_back(tmp_packet);
-       */
+}
 
+
+int main(){
+    vector<struct pack> sent_packets;
+    vector<struct pack> acked_packets;
+
+    string source_port;
+    char filename1[40];
+    char filename2[40];
+
+    cout << "in-file (sent): ";
+    cin.getline(filename1, 40);
+
+    cout << "in-file (ack): ";
+    cin.getline(filename2, 40);
+
+    parse_input(&sent_packets, filename1);
+    parse_input(&acked_packets, filename2);
+
+    //parse file, each packet in vector<struct pack>
     int tot_size = 0;
+
+    //<time, unacked bytes>
+    map<float, int> unack_map;
+
+    for(std::vector<struct pack>::iterator it = sent_packets.begin(); it != sent_packets.end(); ++it){
+        
+    /*
+    unack_map[1.04] = 8;
+    unack_map[2.04] = 7;
+    unack_map[1.05] = 6;
+    unack_map[1.03] = 10;
+*/
+    for(std::map<float, int>::iterator it = unack_map.begin(); it != unack_map.end(); ++it){
+        cout << it->second << endl;
+    }
+    bool base_found = false;
+
+    int seq_nr;
+
+    int big_sent = 0;
+    int big_acked = 0;
+/*
+    //itterate through vector
+    std::vector<struct pack>::iterator it2;
     for(std::vector<struct pack>::iterator it = packets.begin(); it != packets.end(); ++it){
-    //    cout << it->size << endl;
-      //cout << it->time << endl;
-        tot_size += it->size;
-    cout << tot_size << endl;
+        //find base seq
+        if(!base_found && it->port == source_port)
+            base_seq = it->seq_nr;
+
+        if(it->port == source_port){
+            if(big_sent < it->seq_nr)
+                big_sent = it->seq_nr; 
+
+        }else{
+            if(big_acked < it->ack_nr)
+                big_acked = it->ack_nr;
+        }
+
+
+
+
+        //    cout << it->size << endl;
+        //cout << it->time << endl;
+        current = it-seq_nr;
+        size
+            tot_size += it->size;
+        cout << tot_size << endl;
     }
     cout << tot_size << endl;
 
-
+*/
     /*
        char teest[100];
        ack_file >> teest;
@@ -104,3 +149,4 @@ getline(the_file, line);
        */
     return 0;
 }
+
